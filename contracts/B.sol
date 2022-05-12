c604f6cbf220732881b0975fbd3dc23a502f2f17
Massil Achab <massil.achab@gmail.com>
2022-05-12 13:17:35 +0200

    first commit

---
+++ contracts/B.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+interface IA {
+    function num() external view returns (uint256);
+}
+
+contract B {
+    uint256 public num;
+    IA A;
+
+    function set(uint256 _num) public {
+        num = _num;
+    }
+
+    function setA(address addr) public {
+        A = IA(addr);
+    }
+
+    function compute(uint256 n) public returns (uint256) {
+        set(30);
+
+        uint256 tmp = 0;
+        for (uint256 i = 0; i < n; i++) {
+            tmp += num;
+        }
+        return tmp;
+    }
+
+    function computeFromA(address addr, uint256 n) public returns (uint256) {
+        setA(addr);
+
+        uint256 tmp = 0;
+        for (uint256 i = 0; i < n; i++) {
+            tmp += A.num();
+        }
+        return tmp;
+    }
+
+    function compute_1() public returns (uint256) {
+        return compute(1);
+    }
+
+    function computeFromA_1(address addr) public returns (uint256) {
+        return computeFromA(addr, 1);
+    }
+
+    function compute_10() public returns (uint256) {
+        return compute(10);
+    }
+
+    function computeFromA_10(address addr) public returns (uint256) {
+        return computeFromA(addr, 10);
+    }
+
+    function compute_100() public returns (uint256) {
+        return compute(100);
+    }
+
+    function computeFromA_100(address addr) public returns (uint256) {
+        return computeFromA(addr, 100);
+    }
+}
---
+++ test/B.test.js
@@ -0,0 +1,25 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("B", function () {
+    let a;
+    let b;
+  it("deploy stuff", async function () {
+    const AFactory = await ethers.getContractFactory("A");
+    a = await AFactory.deploy();
+    await a.deployed();
+
+    const BFactory = await ethers.getContractFactory("B");
+    b = await BFactory.deploy();
+    await b.deployed();
+  });
+
+  it("call functions", async function () {
+    await b.compute_1();
+    await b.computeFromA_1(a.address);
+    await b.compute_10();
+    await b.computeFromA_10(a.address);
+    await b.compute_100();
+    await b.computeFromA_100(a.address);
+  });
+});
